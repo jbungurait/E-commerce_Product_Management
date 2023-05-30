64971bebb7ea326a3faf22a3add22168c8615db9
jbungurait <120280286+jbungurait@users.noreply.github.com>
2023-05-29 19:27:35 -0700

    changed up models to meet requirements

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,5 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, DECIMAL } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -21,12 +21,15 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.DECIMAL,
       allowNull: false,
+      validate: DECIMAL,
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      defaultValue: 10,
+      validate: Number,
     },
     category_id: {
       type: DataTypes.INTEGER,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
@@ -6,14 +6,21 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     product_id: {
       type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false,
+      references: {
+        model: 'product',
+        key: 'id',
+      }
     },
     tag_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references: {
         model: 'tag',
         key: 'id',
